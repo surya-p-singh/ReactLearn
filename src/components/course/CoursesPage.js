f2f0074d11cc07aff1a71cdfe772e235725a10f6
Surya Singh <surya.singh@thetrainline.com>
2017-02-21 16:38:31 +0000

    Added Courses page

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,3 +1,15 @@
-/**
- * Created by SuryaS on 21/02/2017.
- */
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+
+export default CoursesPage;
+
