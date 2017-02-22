9b45153d11136c8a7a810e1121f7e8ec270a7bf1
Surya Singh <surya.singh@thetrainline.com>
2017-02-22 17:05:15 +0000

    Added CourseList

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,9 +1,20 @@
 import * as types from './actionTypes'
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course){
+
+export function loadCoursesSucess(courses){
   return {
-
-    type:types.CREATE_COURSE,
-    course
+    type:types.LOAD_COURSES_SUCCESS,
+    courses
   };
+}
+
+export function loadCourses(){
+    return function (dispatch) {
+        return courseApi.getAllCourses().then(courses => {
+          dispatch(loadCoursesSucess(courses));
+        }).catch(error =>{
+            throw (error);
+        });
+    }
 }
--- src/components/course/CoursesPage-dispatching-to-redux-3rd-way.js
+++ src/components/course/CoursesPage-dispatching-to-redux-3rd-way.js
@@ -2,23 +2,48 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
-import CourseList from './CourseList'
+
 class CoursesPage extends React.Component {
 
   constructor(props,context){
     super(props,context);
+
+    this.state = {
+      course:{title:null}
+    };
+
+    this.onTitleChange= this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course:course});
+  }
+
+  onClickSave (event){
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow (course, index){
     return <div key={index}>{course.title}</div> ;
   }
-
   render(){
-    const {courses} = this.props;
     return(
       <div>
         <h1>Courses</h1>
-        <CourseList courses={courses} />
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
+        <input
+            type="text"
+            onChange={this.onTitleChange}
+            value={this.state.course.title} />
+
+        <input
+            type="submit"
+            value="Save"
+            onClick={this.onClickSave} />
       </div>
     );
   }
@@ -28,6 +53,8 @@ CoursesPage.propTypes = {
   courses:PropTypes.array.isRequired,
   actions:PropTypes.object.isRequired
 };
+
+
 
 function mapStateToProps(state, ownProps) {
   return{
@@ -43,7 +70,7 @@ function mapDispatchToProps(dispatch)
 }
 
 /*const connectedStateAndProps = connect(mapStateToProps,mapDispatchToProps);
- export default connectedStateAndProps(CoursesPage);*/
+export default connectedStateAndProps(CoursesPage);*/
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
 
